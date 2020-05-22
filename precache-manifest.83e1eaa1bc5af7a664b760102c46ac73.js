e4f1979ec0f6174f347c912838518cd3b75ed9dc
Matt Erickson <matt.erickson@auth0.com>
2020-05-22 12:13:43 -0500

    Updates

---
+++ precache-manifest.83e1eaa1bc5af7a664b760102c46ac73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc40da97994632c29cbc9b954616862",
+    "url": "/time_to_psc_csc/index.html"
+  },
+  {
+    "revision": "faa91399765da91c8a6c",
+    "url": "/time_to_psc_csc/static/css/main.2944017d.chunk.css"
+  },
+  {
+    "revision": "446c1e538676d13f9637",
+    "url": "/time_to_psc_csc/static/js/2.828a3451.chunk.js"
+  },
+  {
+    "revision": "b22554b680d5055d68e4ed5316429c33",
+    "url": "/time_to_psc_csc/static/js/2.828a3451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa91399765da91c8a6c",
+    "url": "/time_to_psc_csc/static/js/main.bda72c39.chunk.js"
+  },
+  {
+    "revision": "2fdb9a190303c9d2ff04",
+    "url": "/time_to_psc_csc/static/js/runtime-main.045b01b6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time_to_psc_csc/precache-manifest.eccbd4bd908311c3cd82bf809fb02265.js"
+  "/time_to_psc_csc/precache-manifest.83e1eaa1bc5af7a664b760102c46ac73.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/time_to_psc_csc/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/time_to_psc_csc/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
