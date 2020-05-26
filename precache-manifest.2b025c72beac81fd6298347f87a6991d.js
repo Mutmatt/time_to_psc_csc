962b500d9b85de70eaf3adc6edb82261fc234d55
Matt Erickson <matt.erickson@auth0.com>
2020-05-26 11:26:15 -0500

    Updates

--- precache-manifest.2b025c72beac81fd6298347f87a6991d.js
+++ precache-manifest.2b025c72beac81fd6298347f87a6991d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50c626dc8fb4a0e0b361c843265f890e",
+    "revision": "54171ea3aab94449fba3a286c0a037b5",
     "url": "/time_to_psc_csc/index.html"
   },
   {
-    "revision": "411c3a8ae2aeabb39644",
+    "revision": "54ad5a20dbcc2ee18c24",
     "url": "/time_to_psc_csc/static/css/main.2944017d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/time_to_psc_csc/static/js/2.44aa44e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "411c3a8ae2aeabb39644",
-    "url": "/time_to_psc_csc/static/js/main.ec1c28ff.chunk.js"
+    "revision": "54ad5a20dbcc2ee18c24",
+    "url": "/time_to_psc_csc/static/js/main.62573efb.chunk.js"
   },
   {
     "revision": "2fdb9a190303c9d2ff04",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time_to_psc_csc/precache-manifest.8d6523b96be76b2da8617f7987ee35b4.js"
+  "/time_to_psc_csc/precache-manifest.2b025c72beac81fd6298347f87a6991d.js"
 );
 
 self.addEventListener('message', (event) => {
